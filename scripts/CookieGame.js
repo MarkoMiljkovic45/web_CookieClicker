dbfe20492c89ca79ff4b569ab425dbeb8a140261
Marko Miljkovic <miljkovicmarko45@gmail.com>
2022-04-09 13:07:38 +0200

    On branch master
     Initial commit
    
     Changes to be committed:
    	new file:   images/cookie.png
    	new file:   main.html
    	new file:   scripts/CookieGame.js
    	new file:   styles/style.css

---
+++ scripts/CookieGame.js
@@ -0,0 +1,43 @@
+class CookieGame {
+    cookieCount;
+    gameIntercalTimer;
+    updateTimerUI;
+    updateHighScoreListUI;
+    resetGameUI;
+
+    constructor() {
+
+    }
+
+    resetGame() {
+
+    }
+
+    startGame() {
+
+    }
+}
+
+class HighScoreManager {
+    getHighScores() {
+
+    }
+
+    updateHighScore() {
+
+    }
+}
+
+class Utils {
+    setDummyHighscores() {
+
+    }
+
+    formatTwoDigits() {
+
+    }
+
+    formatTime() {
+        
+    }
+}
